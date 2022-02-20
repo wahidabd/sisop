af86c520dfbae8f9d6cc4f1029072af736fb98ec
root <root@Legion.localdomain>
2022-02-20 13:18:29 +0700

    add transfer.sh in shell script

--- modul1/findingMentee.sh
+++ modul1/findingMentee.sh
@@ -3,3 +3,6 @@
 #awk -F [:,] '/wahid/ {print "Menteemu " $1 " ada di " $9}' /etc/passwd
 
 awk -F: '{ print "Menteemu "$1 " ada di " $6}' /etc/passwd > pathmenteemunihboss.haha
+
+#transfer.sh
+curl --upload-file /home/wahid/sisop/modul1/pathmenteemunihboss.haha https://transfer.sh/pathmenteemunihboss.haha
