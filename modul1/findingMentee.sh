eadcb3990b54cb98798af99e0491bcd0e22bc9eb
root <root@Legion.localdomain>
2022-02-20 11:44:29 +0700

    add findingMentee

--- modul1/findingMentee.sh
+++ modul1/findingMentee.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-awk -F [:,] '/wahid/ {print "Menteemu " $1 " ada di " $9}' /etc/passwd
+#awk -F [:,] '/wahid/ {print "Menteemu " $1 " ada di " $9}' /etc/passwd
+
+awk -F: '$1 { print "Menteemu "$1 " ada di " $6}' /etc/passwd > pathmenteemunihboss.haha
--- modul1/latihan/findingMentee.sh
+++ modul1/latihan/findingMentee.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-awk -F [:,] '/wahid/ {print "Menteemu " $1 " ada di " $9}' /etc/passwd
+#awk -F [:,] '/wahid/ {print "Menteemu " $1 " ada di " $9}' /etc/passwd
+
+
+ awk -F: '
+        BEGIN {}
+
+        $3 > 100 && $3 < 50000 {printf ("Menteemu " $1 " ada di " $6 "\n") }
+
+        ' /etc/passwd
+
